fd219e1632b666c35bdab97a4c223dec176a42e5
Gaetan PRIOUR <priour.gaetan@gmail.com>
2018-11-07 13:09:28 +0100

    Add 2 version to get and loop through products

--- template-parts/home-slideshow.php
+++ template-parts/home-slideshow.php
@@ -2,25 +2,47 @@
     <div class="carousel-inner">
     <?php
 
-        // Get the latest 3 products from category 'home'
-        $args = array(
-            'post_type' => 'product',
-            'posts_per_page' => 3,
-            'product_cat' => 'home'
-        );
-
-        $loop = new WP_Query($args);
-
         global $first_slide;
+        global $product;
+
         $first_slide = true;
 
-        while ($loop->have_posts()) : $loop->the_post();
-            
-            get_template_part('template-parts/product', 'slideshow');
+        // Get the latest 3 products from category 'home'
 
-        endwhile;
+        // V1 & V2
+        $args = array(
+            'limit' => 3,
+            'category' => 'home',
+        );
 
-        wp_reset_postdata();
+        // V1 - WC_Product_Query
+        // $query = new WC_Product_Query($args);
+        // $products = $query->get_products();
+
+        // V2 - wc_get_products
+        $products = wc_get_products($args);
+
+        foreach ($products as $product) {
+            get_template_part('template-parts/product-bis', 'slideshow');
+        }
+
+        // V3 - WP_Query
+
+        // $args = array(
+        //     'post_type' => 'product',
+        //     'posts_per_page' => 3,
+        //     'product_cat' => 'home'
+        // );
+
+        // $loop = new WP_Query($args);
+
+        // while ($loop->have_posts()) : $loop->the_post();
+
+        //     get_template_part('template-parts/product', 'slideshow');
+
+        // endwhile;
+
+        // wp_reset_postdata();
 
     ?>
     </div>
--- template-parts/product-bis-slideshow.php
+++ template-parts/product-bis-slideshow.php
@@ -1,7 +1,7 @@
 <?php
 
     global $first_slide;
-
+    global $product;
     $class = '';
     
     if ($first_slide === true) {
@@ -13,8 +13,8 @@
 <div class="carousel-item<?php echo $class ?>">
     
     <?php echo woocommerce_get_product_thumbnail(); ?>
-    <h3><?php the_title(); ?></h3>
-    <p><?php the_excerpt(); ?></p>
+    <h3><?php echo $product->get_name(); ?></h3>
+    <p><?php echo $product->get_description(); ?></p>
     <a href="<?php the_permalink(); ?>" class="btn btn-primary">
         Voir
     </a>
