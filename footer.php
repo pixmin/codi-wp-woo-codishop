cc255bcb15220586fbe1c148277d8e3999724f2c
Gaetan PRIOUR <priour.gaetan@gmail.com>
2018-11-07 10:57:13 +0100

    Install Bootstrap v4.1.3

--- footer.php
+++ footer.php
@@ -2,5 +2,6 @@
     <h2>Footer</h2>
 </footer>
 </div><!-- .container -->
+<?php wp_footer(); ?>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -13,8 +13,11 @@ add_filter('woocommerce_enqueue_styles', '__return_empty_array');
 function gp_enqueue_scripts() {
 
     wp_enqueue_style('toaster-style', get_stylesheet_directory_uri() . '/style.css');
+    wp_enqueue_style('gp-bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.min.css');
+
 
     wp_enqueue_script('gp-jquery', get_stylesheet_directory_uri() . '/js/jquery-3.3.1.min.js', array(), '3.3.1', true);
+    wp_enqueue_script('gp-boostrap-js', get_stylesheet_directory_uri() . '/js/bootstrap.min.js', array(), '4.1.3', true);
 
 }
 add_action('wp_enqueue_scripts', 'gp_enqueue_scripts');
