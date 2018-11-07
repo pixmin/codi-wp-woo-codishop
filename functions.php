5a8786471cd12b3997916549a7a3357f3a99129d
Gaetan PRIOUR <priour.gaetan@gmail.com>
2018-11-07 10:46:52 +0100

    Install jQuery v3.3.1

--- functions.php
+++ functions.php
@@ -9,8 +9,12 @@ function customtheme_add_woocommerce_support()
 // Remove WooCommerce CSS
 add_filter('woocommerce_enqueue_styles', '__return_empty_array');
 
-// Add theme style
+// Add theme style and scripts
 function gp_enqueue_scripts() {
+
     wp_enqueue_style('toaster-style', get_stylesheet_directory_uri() . '/style.css');
+
+    wp_enqueue_script('gp-jquery', get_stylesheet_directory_uri() . '/js/jquery-3.3.1.min.js', array(), '3.3.1', true);
+
 }
 add_action('wp_enqueue_scripts', 'gp_enqueue_scripts');
