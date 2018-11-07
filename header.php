61908499a11b568e90acf085dfb88ad963d3a8ed
Gaetan PRIOUR <priour.gaetan@gmail.com>
2018-11-07 14:43:11 +0100

    Make the navbar transparent (remove bootstrap class)

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 
 <header>
     <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light">
             <a class="navbar-brand" href="#"><?php echo get_bloginfo('name'); ?></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
