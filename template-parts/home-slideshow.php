1abce5229fcbdaef3b6ad54963ee0229644e64b3
Gaetan PRIOUR <priour.gaetan@gmail.com>
2018-11-07 11:14:18 +0100

    Plug products in carousel

--- template-parts/home-slideshow.php
+++ template-parts/home-slideshow.php
@@ -1,20 +1,35 @@
-<?php
+<div id="product-carousel" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+    <?php
 
-// Get the latest 3 products from category 'home'
-$args = array(
-    'post_type' => 'product',
-    'posts_per_page' => 3,
-    'product_cat' => 'home'
-);
+        // Get the latest 3 products from category 'home'
+        $args = array(
+            'post_type' => 'product',
+            'posts_per_page' => 3,
+            'product_cat' => 'home'
+        );
 
-$loop = new WP_Query($args);
+        $loop = new WP_Query($args);
 
-while ($loop->have_posts()) : $loop->the_post();
-    
-    get_template_part('template-parts/product', 'slideshow');
+        global $first_slide;
+        $first_slide = true;
 
-endwhile;
+        while ($loop->have_posts()) : $loop->the_post();
+            
+            get_template_part('template-parts/product', 'slideshow');
 
-wp_reset_postdata();
+        endwhile;
 
-?>
+        wp_reset_postdata();
+
+    ?>
+    </div>
+    <a class="carousel-control-prev" href="#product-carousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#product-carousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
--- template-parts/product-slideshow.php
+++ template-parts/product-slideshow.php
@@ -1,4 +1,16 @@
-<article>
+<?php
+
+    global $first_slide;
+
+    $class = '';
+    
+    if ($first_slide === true) {
+        $class = ' active';
+        $first_slide = false;
+    }
+
+?>
+<div class="carousel-item<?php echo $class ?>">
     
     <a href="<?php the_permalink(); ?>">
     <?php
@@ -9,4 +21,4 @@
     ?>
     </a>
 
-</article>
+</div>
