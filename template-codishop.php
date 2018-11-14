1fc6a246f4484f05f4a28df7b035e8740d52a3b5
Gaetan PRIOUR <priour.gaetan@gmail.com>
2018-11-14 10:25:57 +0100

    Remove unused template (moved to header)

--- template-codishop.php
+++ template-codishop.php
@@ -6,8 +6,6 @@
 
 get_header();
 
-// get_template_part('template-parts/home', 'slideshow');
-
 get_template_part('template-parts/menu', 'categories');
 
 get_template_part('template-parts/home', 'products');
