d6f7cafa3cb6aa8dfbd1dcdabf22b69568bbc7e2
Gaetan PRIOUR <priour.gaetan@gmail.com>
2018-11-14 11:14:43 +0100

    Add second option to get page content in footer

--- template-parts/footer-about.php
+++ template-parts/footer-about.php
@@ -2,8 +2,14 @@
     <h4>About</h4>
     <?php
 
-    // Content from About page
+    // Content from About page (2 options)
+    
+    // By name:
+    // $post = get_page_by_title('About');
+    
+    // By id:
     $post = get_post(57);
+
     echo apply_filters('the_exceprt', $post->post_excerpt);
 
     ?>
